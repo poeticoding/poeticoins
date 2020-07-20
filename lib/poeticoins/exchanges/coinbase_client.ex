16b56bdf7864ec925b6d1ad5f3dc7725c7f7c1a8
Alvise Susmel <alvise@poeticoding.com>
2020-07-20 13:06:32 +0200

    subscribe

--- lib/poeticoins/exchanges/coinbase_client.ex
+++ lib/poeticoins/exchanges/coinbase_client.ex
@@ -24,4 +24,30 @@ defmodule Poeticoins.Exchanges.CoinbaseClient do
     {:ok, conn} = :gun.open(server_host(), server_port(), %{protocols: [:http]})
     %{state | conn: conn}
   end
+
+  def handle_info({:gun_up, conn, :http}, %{conn: conn}=state) do
+    :gun.ws_upgrade(conn, "/")
+    {:noreply, state}
+  end
+
+  def handle_info({:gun_upgrade, conn, _ref, ["websocket"], _headers},
+                  %{conn: conn}=state)
+  do
+    subscribe(state)
+    {:noreply, state}
+  end
+
+  defp subscribe(state) do
+    subscription_frames(state.currency_pairs)
+    |> Enum.each(&:gun.ws_send(state.conn, &1))
+  end
+
+  defp subscription_frames(currency_pairs) do
+    msg = %{
+      "type" => "subscribe",
+      "product_ids" => currency_pairs,
+      "channels" => ["ticker"]
+    } |> Jason.encode!()
+    [{:text, msg}]
+  end
 end
