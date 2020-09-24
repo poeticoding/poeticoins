00921534928a93c3253196ed9d727fd54fe9c1db
Alvise Susmel <alvise@poeticoding.com>
2020-09-24 13:25:51 +0200

    Poeticoins exposes subscribe and unsubscribe functions

--- lib/poeticoins.ex
+++ lib/poeticoins.ex
@@ -1,9 +1,8 @@
 defmodule Poeticoins do
-  @moduledoc """
-  Poeticoins keeps the contexts that define your domain
-  and business logic.
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  defdelegate subscribe_to_trades(product),
+    to: Poeticoins.Exchanges, as: :subscribe
+
+  defdelegate unsubcribe_from_trades(product),
+    to: Poeticoins.Exchanges, as: :unsubscribe
 end
