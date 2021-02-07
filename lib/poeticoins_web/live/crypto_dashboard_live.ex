eedd1a3f4bb2809f8dda9578e0d7bc77240d98b0
Alvise Susmel <alvise@poeticoding.com>
2021-02-07 17:08:31 +0100

    stateful product component

--- lib/poeticoins_web/live/crypto_dashboard_live.ex
+++ lib/poeticoins_web/live/crypto_dashboard_live.ex
@@ -4,7 +4,7 @@ defmodule PoeticoinsWeb.CryptoDashboardLive do
   import PoeticoinsWeb.ProductHelpers
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, trades: %{}, products: [])
+    socket = assign(socket, products: [])
     {:ok, socket}
   end
 
@@ -36,52 +36,25 @@ defmodule PoeticoinsWeb.CryptoDashboardLive do
     </div>
 
     <div class="product-components">
-      <%= for product <- @products, trade = @trades[product] do%>
-        <div class="product-component">
-          <div class="currency-container">
-            <img class="icon" src="<%= crypto_icon(@socket, product) %>" />
-            <div class="crypto-name">
-              <%= crypto_name(product) %>
-            </div>
-          </div>
-
-          <div class="price-container">
-            <ul class="fiat-symbols">
-              <%= for fiat <- fiat_symbols() do %>
-                <li class="
-                <%= if fiat_symbol(product) == fiat, do: "active" %>
-                  "><%= fiat %></li>
-              <% end %>
-          </ul>
-
-            <div class="price">
-              <%= trade.price %>
-              <%= fiat_character(product) %>
-            </div>
-          </div>
-
-          <div class="exchange-name">
-            <%= product.exchange_name %>
-          </div>
-
-          <div class="trade-time">
-            <%= human_datetime(trade.traded_at) %>
-          </div>
-        </div>
+      <%= for product <- @products do%>
+        <%= live_component @socket, PoeticoinsWeb.ProductComponent,
+                          id: product %>
       <% end %>
     </div>
     """
   end
 
   def handle_info({:new_trade, trade}, socket) do
-    socket = update(socket, :trades, &Map.put(&1, trade.product, trade))
+    send_update(PoeticoinsWeb.ProductComponent,
+      id: trade.product,
+      trade: trade
+    )
 
     {:noreply, socket}
   end
 
   def handle_event("add-product", %{"product_id" => product_id} = _params, socket) do
-    [exchange_name, currency_pair] = String.split(product_id, ":")
-    product = Product.new(exchange_name, currency_pair)
+    product = product_from_string(product_id)
     socket = maybe_add_product(socket, product)
     {:noreply, socket}
   end
@@ -90,15 +63,15 @@ defmodule PoeticoinsWeb.CryptoDashboardLive do
     {:noreply, socket}
   end
 
-  def handle_event("filter-products", %{"search" => search}, socket) do
-    products =
-      Poeticoins.available_products()
-      |> Enum.filter(fn product ->
-        String.downcase(product.exchange_name) =~ String.downcase(search) or
-          String.downcase(product.currency_pair) =~ String.downcase(search)
-      end)
+  def handle_event("remove-product", %{"product-id" => product_id} = _params, socket) do
+    product = product_from_string(product_id)
+    socket = update(socket, :products, &List.delete(&1, product))
+    {:noreply, socket}
+  end
 
-    {:noreply, assign(socket, :products, products)}
+  defp product_from_string(product_id) do
+    [exchange_name, currency_pair] = String.split(product_id, ":")
+    Product.new(exchange_name, currency_pair)
   end
 
   def add_product(socket, product) do
@@ -106,10 +79,6 @@ defmodule PoeticoinsWeb.CryptoDashboardLive do
 
     socket
     |> update(:products, &(&1 ++ [product]))
-    |> update(:trades, fn trades ->
-      trade = Poeticoins.get_last_trade(product)
-      Map.put(trades, product, trade)
-    end)
   end
 
   @spec maybe_add_product(Phoenix.LiveView.Socket.t(), Product.t()) :: Phoenix.LiveView.Socket.t()
@@ -117,12 +86,8 @@ defmodule PoeticoinsWeb.CryptoDashboardLive do
     if product not in socket.assigns.products do
       socket
       |> add_product(product)
-      |> put_flash(
-        :info,
-        "#{product.exchange_name} - #{product.currency_pair} added successfully"
-      )
     else
-      put_flash(socket, :error, "The product was already added")
+      socket
     end
   end
 
