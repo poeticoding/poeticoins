1906dd29398b0b6b5fd602f615ed80b651d1da57
Alvise Susmel <alvise@poeticoding.com>
2021-02-18 13:40:23 +0100

    show correct time based on browser timezone

--- lib/poeticoins_web/product_helpers.ex
+++ lib/poeticoins_web/product_helpers.ex
@@ -5,8 +5,10 @@ defmodule PoeticoinsWeb.ProductHelpers do
     ["eur", "usd"]
   end
 
-  def human_datetime(datetime) do
-    Calendar.strftime(datetime, "%b %d, %Y   %H:%M:%S")
+  def human_datetime(datetime, timezone \\ "UTC") do
+    datetime
+    |> DateTime.shift_zone!(timezone)
+    |> Calendar.strftime("%b %d, %Y   %H:%M:%S")
   end
 
   def crypto_icon(conn, product) do
