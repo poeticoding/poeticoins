0888f4051441bc462d06e946588d5437c22a632a
elay12 <layeddie@gmail.com>
2022-01-05 15:16:38 +0000

    fixed missing terminator on end of line 13 in prod.exs - ],

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :poeticoins, PoeticoinsWeb.Endpoint
-  url: [host: "poeticoins-elay.gigalixirapp.com", port: 80
+  url: [host: "poeticoins-elay.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
