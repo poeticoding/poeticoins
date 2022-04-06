2705b9add8655423058782fc5a5920f60131c0e1
Alvise Susmel <alvise@poeticoding.com>
2022-04-06 18:47:03 +0200

    updated to Phoenix 1.6 and LiveView 0.17

--- mix.exs
+++ mix.exs
@@ -33,21 +33,20 @@ defmodule Poeticoins.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.3"},
-      {:phoenix_live_view, "~> 0.13.0"},
+      {:phoenix, "~> 1.6.6"},
+      {:phoenix_live_view, "~> 0.17.7"},
       {:floki, ">= 0.0.0", only: :test},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:phoenix_html, "~> 3.2"},
+      {:phoenix_live_reload, "~> 1.3", only: :dev},
+      {:phoenix_live_dashboard, "~> 0.6.5"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-
+      {:jason, "~> 1.1"},
+      {:plug_cowboy, "~> 2.5"},
       {:gun, "~> 1.3"},
-      {:cowlib, "~> 2.9.1", override: true},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:cowlib, "~> 2.11", override: true},
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false}
     ]
   end
 
